ef9449c1a748fc6f97d968d96b9cad1b196ad957
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-08-11 14:47:34 +0200

    Create main.php

---
+++ main.php
@@ -0,0 +1,33 @@
+<?php
+
+$projectFolder = __DIR__ . DIRECTORY_SEPARATOR
+  
+require $projectFolder . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+use PierreMiniggio\ConfigProvider\ConfigProvider;
+use PierreMiniggio\DatabaseConnection\DatabaseConnection;
+use PierreMiniggio\DatabaseFetcher\DatabaseFetcher;
+use PierreMiniggio\EarthSandwichTelegramBot\App;
+
+$configProvider = new ConfigProvider($projectFolder);
+$config = $configProvider->get();
+
+if (! isset($config['bot'])) {
+    throw new Exception('Missing bot config');
+}
+
+try {
+    exit((new App(
+        $config['bot'],
+        new DatabaseFetcher(new DatabaseConnection(
+            $dbConfig['host'],
+            $dbConfig['database'],
+            $dbConfig['username'],
+            $dbConfig['password'],
+            DatabaseConnection::UTF8_MB4
+        ))
+    ))->run());
+} catch (Throwable $e) {
+    echo get_class($e) . ' : ' . $e->getMessage();
+    exit;
+}
