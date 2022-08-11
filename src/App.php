1f9ac89a734f8a95c094a03be168e5faef433e43
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-08-11 14:50:22 +0200

    Create App.php

---
+++ src/App.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EarthSandwichTelegramBot;
+
+use DateTime;
+use PierreMiniggio\DatabaseFetcher\DatabaseFetcher;
+
+class App
+{
+    public function __construct(
+        private string $bot,
+        private DatabaseFetcher $fetcher
+    )
+    {
+    }
+  
+    public function run(): int
+    {
+        var_dump($this->bot);
+    }
+}
